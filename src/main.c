1b9cda28bfeaa740291b04ffca00d5d3de7b177b
MstrssZ <nonofyobiznis@gmail.com>
2025-10-06 18:53:57 -0400

    add verbose flag

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <getopt.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
@@ -9,6 +10,7 @@ void usage(FILE *stream, char *prgm){
   fprintf(stream, "Options:\n");
   fprintf(stream, "\t-h       \tPrint usage\n");
   fprintf(stream, "\t-n <name>\tCustom name for the main file\n");
+  fprintf(stream, "\t-v       \tPrint logging info\n");
 }
 
 int main(int argc, char **argv){
@@ -16,14 +18,15 @@ int main(int argc, char **argv){
   int opt;
   char *prgm = argv[0];
   char *main_name = "main";
-  
+  bool verbose = false;
+
   if (argc < 2){
     fprintf(stderr, "Error: no project name given\n");
     usage(stderr, prgm);
     exit(1);
   }
 
-  while ((opt = getopt(argc, argv, "n:h")) != -1){
+  while ((opt = getopt(argc, argv, "n:hv")) != -1){
     switch (opt){
     case 'h':
       usage(stdout, prgm);
@@ -31,13 +34,15 @@ int main(int argc, char **argv){
     case 'n':
       main_name = optarg;
       break;
+    case 'v':
+      verbose = true;
     case '?':
       usage(stderr, prgm);
       exit(1);
       break;
-    }}    
+    }}
   char *project_name = argv[optind];
- 
+
   fprintf(stdout, "project name: %s\n", project_name);
 
   fprintf(stdout, "main name: %s\n", main_name);
@@ -45,10 +50,14 @@ int main(int argc, char **argv){
   struct stat st = {0};
   if (stat(project_name, &st) == -1){
     mkdir(project_name, 0755); //0755 is the standard permissions for a directory
-    printf("INFO: Made folder: %s\n", project_name);
+    if (verbose) {
+      printf("INFO: Made folder: %s\n", project_name);
+    }
+  } else {
+    printf("INFO: Folder '%s' exists\n", project_name);
   }
 
 
-  
+
   return 0;
 }
